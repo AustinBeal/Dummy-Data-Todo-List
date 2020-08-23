eb969b4d9a0d4d5f2539747c97ef870be8227159
AustinBeal <austinbeal89@gmail.com>
2020-08-23 17:07:37 -0500

    solved major problems with the populate and reset buttons and styled page

--- app.js
+++ app.js
@@ -23,106 +23,39 @@ const logTodos = () => {
 }
 
 const populateTodos = () => {
-  //access the ol tag to insert list items
+  
   var ol = document.getElementById('todo-list')
   
-  for (let index = 0; index < arrayOfTodos.length; index++) {
+  for (let index = 0; index < 20; index++) {
 
-  //created list item
   var listItem = document.createElement("li");
-
-//loop through attay to poopulate don with each items title
   const elementTitle = arrayOfTodos[index].title
-  
-
-//created text to go inside <li> text </li> 
   var listItemText = document.createTextNode(elementTitle);
-
-//append tecxt to the listItem
   listItem.appendChild(listItemText);
-  
-//append to ol li text li ol
   ol.appendChild(listItem)
-  }
+  listItem.setAttribute("id","liid")
+ 
+  
+
+var isComplete = arrayOfTodos[index].completed;
+  
+ for (let index = 0; index < 20; index++) {
+   
+    if (isComplete){
+      (listItem.style.backgroundColor = "lightpink")
+   }
+   else{
+    (listItem.style.backgroundColor = "#FF0120")
+   }
+
+console.log(isComplete)
+}
+}
 }
 
-
-//Then capture the ol item into a variable (getElementById)
-
-
-//createElement to make a new li
-//createText inside the li using the title property.
-// Now append the text to the new element.
-// Then append the element to the ol element.
-// Put all of that inside your populateTodos function.
-
-
-
-
-
-
-
-//JUANS WORK
-
-// const populateUserSelection = () => {
-//   console.log('hey im working')
-//   const inputElement = document.getElementById('input')
-//   const inputElement = document.querySelector('input')
-//   console.log('inputElement:' inputElement)
-//   let inputValue = inputElement[0].value
-//   //clear array 
-//   //filter userId  of the line number in the input
-//   todoByUserId = arrayOfTodos.filter(todo =>{
-//     console.log('todo.userId', todo)
-//     if (todo.userid === inputValue
-//       return todo
-//     }
-//     )
-//   //store the new Array
-// }
-// const removeTodos = {} => {
-//   //remove todos
-//   //show todos
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// }
-
-  // const populateTodos = () => {
-  // access the ol tag to insert list items
- // var ol = document.getElementById('todo-list')
-
-  // loop through array to populate dom with each items title
- // for (let index = 0; index < arrayOfTodos.length; index++) {
-    // created listItem
-    //var listItem = document.createElement("li"); 
-    // get array item title
-    //const elementTitle = arrayOfTodos[index].title
-    // created text to go inside <li> text </li>
-   // var listItemText = document.createTextNode(elementTitle); 
-
-    // appended the text to the listItem
-   // listItem.appendChild(listItemText);
-
-    // append to <ol> <li> Hi there and greetings! </li> </ol>
-   // ol.appendChild(listItem)
- // }
-//}
+let resetTodos = () => {   
+  for (let i=0; i < 500; i++){   
+    var myobj = document.getElementById("liid");   
+    myobj.remove();
+  } 
+}
